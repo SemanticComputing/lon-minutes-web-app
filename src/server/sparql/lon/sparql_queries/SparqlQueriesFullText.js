7823b62f563e3e5215a55a91cd4e163ce977845c
Petri Leskinen <petri.leskinen@aalto.fi>
2025-09-30 11:48:27 +0300

    Full text search: show text score

--- src/server/sparql/lon/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/lon/sparql_queries/SparqlQueriesFullText.js
@@ -2,7 +2,7 @@ export const fullTextSearchProperties = `
 VALUES (?type__id ?type__label ?pagetype ?value_prop)
 {
   (crm:E21_Person "Person" "/actors" :number_of_references)
-  (crm:E74_Group "Group" "/actors" :number_of_references)
+  (crm:E74_Group "Group" "/organizations" :number_of_references)
   (crm:E53_Place "Place" "/places" :number_of_references)
   (skos:Concept "Concept" "/concepts" :number_of_references)
 }
